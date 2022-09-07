0bebb25b28c540e601e98891453b2679813e4b7a
Carlo D'Abbraccio <113054931+carlodab@users.noreply.github.com>
2022-09-07 20:24:44 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,197 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="author" content="Lenovo" >
+
+<title>VaporSpirit</title>
+
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+<!-- Multiheader https://bbbootstrap.com/snippets/e-commerce-menu-header-using-html-css-21940848 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.3/TweenMax.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+ 
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/bootstrap-select.min.js"></script>
+
+<!-- (Optional) Latest compiled and minified JavaScript translation files -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.14/dist/js/i18n/defaults-*.min.js"></script>
+<!-- jQuery -->
+<script src="js/jquery-2.0.0.min.js" type="text/javascript"></script>
+
+<!-- Bootstrap4 files-->
+<script src="js/bootstrap.bundle.min.js" type="text/javascript"></script>
+<link href="css/bootstrap-custom.css" rel="stylesheet" type="text/css"/>
+
+<!-- Font awesome 5 -->
+<link href="fonts/fontawesome/css/fontawesome-all.min.css" type="text/css" rel="stylesheet">
+
+<!-- plugin: fancybox  -->
+<script src="plugins/fancybox/fancybox.min.js" type="text/javascript"></script>
+<link href="plugins/fancybox/fancybox.min.css" type="text/css" rel="stylesheet">
+
+<!-- plugin: owl carousel  -->
+<link href="plugins/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+<link href="plugins/owlcarousel/assets/owl.theme.default.css" rel="stylesheet">
+<script src="plugins/owlcarousel/owl.carousel.min.js"></script>
+
+<!-- custom style -->
+<link href="css/uikit.css" rel="stylesheet" type="text/css"/>
+<link href="css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
+
+<!-- custom javascript -->
+<script src="js/script.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+/// some script
+
+// jquery ready start
+$(document).ready(function() {
+	// jQuery code
+
+}); 
+// jquery end
+
+
+</script>
+
+</head>
+<body>
+<!-- Supercontainer für Multiheader// -->
+<div class="super_container">
+	<header class="header h-7"style="height: 40px">
+<!-- properties hardcoded local wie farbe und schrift und hintergrundfarbe// -->
+		<div class= "top_bar text-white h-5" style="background-color: #FF1493;height: 20px">
+			<div class="container">
+				<div class="row">
+					<div class="col d-flex flex-row">
+                 
+						<div class="top_bar_contact_item"> 
+						<p style="text-center">
+						20% NEUKUNDEN-Rabatt Rabattcode: MYCBD (ausser SALE Artikel)&nbsp 40% Hash-Rabatt Rabattcode: HASH40 Schweiz: Bis 17:30 Uhr bestellt und schon am nächsten Tag da.  GRATIS Versand ab 100.-
+​                  </p></div>
+				
+				
+			</div> <!-- lauftext ende der ersten topbar// -->
+							<!-- divbox.// -->
+						</div>
+					</div>
+				</div>
+			</div>		
+		</div>
+		<!-- Top Bar -->
+
+
+			</div>		
+		</div>
+
+<header class="section-header">
+<section class="header-main">
+	<div class="container">
+<div class="row">
+	<div class="col-lg-3">
+	<div class="brand-wrap">
+		<img class="logo" src="logov.jpg" width="90%";
+  height="auto">
+		<h2 class="logo-text"></h2>
+	</div> <!-- brand-wrap.// -->
+	</div>
+	<h3>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+	</h3>
+	<div class="col-lg-2 col-sm-4 pull-right">
+		<form action="#" class="search-wrap" >
+			<div class="input-group">
+			    <input type="text" class="form-control" placeholder="Search" style="width:30px">
+			    <div class="input-group-append">
+			      <button class="btn btn-primary" type="submit">
+			        <i class="fa fa-search"></i>
+			      </button>
+			    </div>
+		    </div>
+		</form> <!-- search-wrap .end// -->
+	</div> <!-- col.// -->
+	<div class="col-lg-3 col-sm-6">
+		<div class="widgets-wrap d-flex justify-content-end">
+			<div class="widget-header">
+				<a href="#" class="icontext">
+					<div class="icon-wrap icon-xs bg2 round text-secondary"><i class="fa fa-shopping-cart"></i></div>
+					<div class="text-wrap">
+						<small>Warenkorb</small>
+						<span>3 Items</span>
+					</div>
+				</a>
+			</div> <!-- widget .// -->
+			<div class="widget-header dropdown">
+				<a href="#" class="ml-3 icontext" data-toggle="dropdown" data-offset="20,10">
+					<div class="icon-wrap icon-xs bg2 round text-secondary"><i class="fa fa-user"></i></div>
+					<div class="text-wrap">
+						<small>Mein Konto</small>
+						<span>Anmelden <i class="fa fa-caret-down"></i></span>
+					</div>
+				</a>
+				<div class="dropdown-menu dropdown-menu-right">
+					<form class="px-4 py-3">
+						<div class="form-group">
+						  <label>Email</label>
+						  <input type="email" class="form-control" placeholder="email@example.com">
+						</div>
+						<div class="form-group">
+						  <label>Passwort</label>
+						  <input type="password" class="form-control" placeholder="Password">
+						</div>
+						<button type="submit" class="btn btn-primary">Anmelden</button>
+						</form>
+						<hr class="dropdown-divider">
+						<a class="dropdown-item" href="#">Kein Account? Registrieren Sie sich!</a>
+						<a class="dropdown-item" href="#">Passwort vergessen?</a>
+				</div> <!--  dropdown-menu .// -->
+			</div> <!-- widget  dropdown.// -->
+		</div>	<!-- widgets-wrap.// -->	
+	</div> <!-- col.// -->
+</div> <!-- row.// -->
+	</div> <!-- container.// -->
+</section> <!-- header-main .// -->
+
+<div class="parent container d-flex justify-content-center align-items-center h-100">
+<nav class="navbar navbar-expand-lg navbar-light bg-white items-center">
+  <div class="container center">
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav" aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon text-center"></span>
+    </button>
+
+    <div class="collapse navbar-collapse w-100 dual-collapse2 order-1 order-md-0" id="main_nav" style="background-color: white;">
+      <ul class="navbar-nav text-center">
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1">Startseite</button>
+        </li>
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1">Aktionen</button>
+        </li>
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1">Neu im Sortiment</button>
+        </li>
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1">News und Trends</button>
+        </li>
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1" >Shop CH</button>
+        </li>
+        <li class="nav-item">
+<button type="button" class="btn bg-white mr-1">Shop EU</button>
+        </li>
+                <li class="nav-item">
+<button type="button" class="btn btn-danger mr-1">%SALES</button>
+        </li>
+      </ul>
+    </div> <!-- collapse .// -->
+  </div> <!-- container .// -->
+</nav>
+</div>
+<?php get_header(); ?>
+</header> <!-- section-header.// -->
+<?php get_header(); ?>
+</header>
+</div><!-- Supercontainer.// -->
